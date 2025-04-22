55ac56e2a720923d071b8575ee4f98175e2c8287
abiduke <abiduke@umich.edu>
2025-04-22 17:07:16 -0400

    added files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lab03
 EECS402 Discussion / Lab 03
+
+## Errors & Debugging, Arrays
---
+++ uninitialized.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x;
+    if(x == 5){
+        cout << "hello" << endl;
+    }
+    else {
+        cout << "goodbye" << endl;
+    }
+    return 0;
+
+}
